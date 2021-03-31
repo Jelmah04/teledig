3bb43dd198417eeeda575bdc56b691961c53f79f
Jelmah04 <Jelmah04@gmail.com>
2021-03-31 05:43:02 +0200

    ready for heroku

--- fundme/settings.py
+++ fundme/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'uifakkma7epple%p+7ak_bd$i+!^xuw8ojte&sh^uw=#r)t(7g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
