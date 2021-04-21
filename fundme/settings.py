2a6d09611e2d53d1709cbc703dea388fb746647b
Jelmah04 <Jelmah04@gmail.com>
2021-04-21 14:19:35 +0200

    *settings.py removed

--- fundme/settings.py
+++ fundme/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'uifakkma7epple%p+7ak_bd$i+!^xuw8ojte&sh^uw=#r)t(7g'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ["03c330f47917.ngrok.io"]
+# ALLOWED_HOSTS = ["81174dcffeb6.ngrok.io"]
 # ALLOWED_HOSTS = ["agentshub.herokuapp.com", "127.0.0.1"]
 
 
